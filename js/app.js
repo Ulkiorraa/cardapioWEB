cb65af7067977ebe139a7ed0ddf767684a915c71
Fellipe J. P. Oliveira <fellipe_ulkiorra@live.com>
2023-08-27 13:15:19 -0300

    cardapio botão ver mais implementado

--- js/app.js
+++ js/app.js
@@ -12,28 +12,45 @@ cardapio.eventos = {
 
 cardapio.metodos = {
   //obtém a lista de itens do cardapio
-  obterItensCardapio: (categoria = 'burgers') => {
+  obterItensCardapio: (categoria = "burgers", vermais = false) => {
     var filtro = MENU[categoria];
 
-    //limpa o cardapio para troca de menus
-    $("#itensMenu").html('')
+    //limpa o cardapio para troca de menus mas não limpa em caso de clicar em ver mais
+    if (!vermais) {
+      $("#itensMenu").html("");
+      $("#btnVerMais").removeClass('hidden');
+    }
 
     //for it do jquery, usando regex para item global
     $.each(filtro, (i, e) => {
       let temp = cardapio.templates.item
         .replace(/\${img}/g, e.img)
         .replace(/\${name}/g, e.name)
-        .replace(/\${price}/g, e.price.toFixed(2).replace('.', ','));
+        .replace(/\${price}/g, e.price.toFixed(2).replace(".", ","));
 
-      $("#itensMenu").append(temp);
+      //ver mais foi clicado
+      if (vermais && i >= 8 && i < 12) {
+        $("#itensMenu").append(temp);
+      }
+
+      //paginação inicial (8 itens)
+      if (!vermais && i < 8) {
+        $("#itensMenu").append(temp);
+      }
     });
 
     //remove active do item
-    $(".container-menu a").removeClass('active');
+    $(".container-menu a").removeClass("active");
 
     //seta o menu escolhido para ativo
-    $("#menu-" + categoria).addClass('active')
+    $("#menu-" + categoria).addClass("active");
+  },
 
+  //botão ver mais click
+  verMais: () => {
+    var ativo = $(".container-menu a.active").attr('id').split('menu-')[1]; //pega o id ex: menu-burger separa em [menu-] [burger] 0 e 1 e escolhe o 1 que é o nome da categoria ativa
+    cardapio.metodos.obterItensCardapio(ativo, true);
+    $("#btnVerMais").addClass('hidden');
   },
 };
 
